b02bb567177865515240bcb236d47c416627d7c3
plexiform <haaris.nyc@gmail.com>
2022-01-18 15:54:46 -0500

    added horizontal lines and changed a few colors

---
+++ src/content/scientific_modeling.md
@@ -0,0 +1,6 @@
+---
+title: "Scientific Modeling"
+date: 2022-01-15
+tags: []
+published: true
+---
--- src/pages/telemetry.js
+++ src/pages/telemetry.js
@@ -5,6 +5,11 @@ import heatclock from '../../storage/heat_clock.png'
 export default () => {
   return (
     <Layout>
+      emacs -> org-mode -> clock time -> export to csv -> import in Rstudio -> run cronjob to generate heatmap + update image file
+      <h1>
+        2020 –
+      </h1>
+      <h1>2021</h1>
       <img src={heatclock}></img>
       <iframe src="https://www.beeminder.com/widget?slug=punctual_ritual&username=excursos" height="185px" width="230px" frameborder="0px" ></iframe>
       <iframe src="https://www.beeminder.com/widget?slug=weightloss&username=excursos" height="185px" width="230px" frameborder="0px" ></iframe>
